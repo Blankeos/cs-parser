4592c80ff8802047c7f0e3314beceb61f612371a
Blankeos <carloantonioct@gmail.com>
2022-10-10 21:24:37 +0800

    Initial progress on tokenization (lexical analysis)

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CSParser
+{
+    enum TokenEnum
+    {
+        NUMBER,
+        PUNCTUATION,
+        OPERATOR,
+    }
+    struct Token
+    {
+        public TokenEnum tokenType;
+        public string value;
+
+        public Token(TokenEnum tokenType, string value)
+        {
+            this.tokenType = tokenType;
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"({this.tokenType}, {this.value})";
+        }
+    }
+    class Parser
+    {
+        public static List<Token> Lex(string charStream)
+        {
+            List<Token> tokens = new List<Token>();
+            for (int i = 0; i < charStream.Length; i++)
+            {
+                char c = charStream[i];
+
+                if (c == ' ' || c == '\n') continue;
+                else if (Regex.Match(c.ToString(), @"\+|-|\*|\/").Success)
+                {
+                    tokens.Add(new Token(TokenEnum.OPERATOR, c.ToString()));
+                }
+                else if (Regex.Match(c.ToString(), @"[0-9]").Success)
+                {
+                    string number = "";
+                    while (Regex.Match(c.ToString(), @"[0-9]").Success && i < charStream.Length)
+                    {
+                        number += c;
+                        i++;
+                        if (i >= charStream.Length) break;
+                        c = charStream[i];
+                    }
+                    tokens.Add(new Token(TokenEnum.NUMBER, number));
+                }
+            }
+            return tokens;
+        }
+
+        public static void PrintTokens(List<Token> tokens)
+        {
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                Console.WriteLine(tokens[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("== Parser in C# by Taleon, Elizalde, Rubinos ==");
+            List<Token> tokens = Parser.Lex("3 + 4 - 500");
+            Parser.PrintTokens(tokens);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Parser in C#
+
+A parser implemented in C# as a final requirement for my Programming Languages class for Dr. Felipe P. Vista IV.
+
+```
+Written by:
+- Carlo Antonio T. Taleon
+- Glecy S. Elizalde
+- Christopher Joseph T. Rubinos
+```
+
+## Get started
+
+> Make sure to install [.Net SDK](https://dotnet.microsoft.com/en-us/download) if you haven't yet
+
+1. Clone this repo and change your directory
+
+```sh
+git clone <repolink>
+cd parser-csharp
+```
+
+2. Run the project in your terminal
+
+```
+dotnet run
+```
